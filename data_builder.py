4df4788d0957e96389c022f3b65d2fe9b1c63b36
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-08 00:00:01 +0530

    dropped more efficiently

--- data_builder.py
+++ data_builder.py
@@ -11,7 +11,7 @@ def merge_datasets():
     return merged_df
 
 def remove_duplicates(df):
-    cols_to_drop = ['movie_title', 'plot_keywords', 'movie_imdb_link', 'language', 'country_x', 'country_y', 'content_rating', 'color', 'duration', 'facenumber_in_poster', 'movie_facebook_likes', 'num_voted_users', 'num_user_for_reviews', 'num_critic_for_reviews', 'title_year', 'aspect_ratio', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_2_facebook_likes', 'actor_1_facebook_likes', 'cast_total_facebook_likes', 'company', 'released', 'votes', 'rating', 'writer', 'year', 'runtime', 'director_name', 'genres', 'imdb_score', 'star', 'actor_3_name']
+    cols_to_drop = ['movie_title', 'plot_keywords', 'movie_imdb_link', 'language', 'country_x', 'country_y', 'content_rating', 'color', 'duration', 'facenumber_in_poster', 'movie_facebook_likes', 'num_voted_users', 'num_user_for_reviews', 'num_critic_for_reviews', 'title_year', 'aspect_ratio', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_2_facebook_likes', 'actor_1_facebook_likes', 'cast_total_facebook_likes', 'company', 'released', 'votes', 'rating', 'writer', 'year', 'runtime', 'director_name', 'genres', 'imdb_score', 'star', 'actor_3_name', 'budget_x']
     df.drop(cols_to_drop, axis=1, inplace=True)
 
     df.drop_duplicates(subset=['name'], inplace=True)
@@ -37,6 +37,10 @@ def remove_incomplete_rows(df):
     df.dropna(subset=cols_to_check, how='any', inplace=True)
     return df
 
+def remove_columns(df, cols):
+    df.drop(cols, axis=1, inplace=True)
+    return df
+
 def write_to_csv(df, filename):
     df.to_csv(filename, index=False)
 
@@ -45,6 +49,7 @@ def main():
     cleaned_df = remove_duplicates(merged_df)
     compared_df = add_gross_column(cleaned_df)
     dropped_df = add_budget_column(compared_df)
+    dropped_df = remove_columns(dropped_df, ['gross_x', 'gross_y', 'budget_x', 'budget_y'])
     final_df = remove_incomplete_rows(dropped_df)
     write_to_csv(final_df, 'final_dataset.csv')
 
