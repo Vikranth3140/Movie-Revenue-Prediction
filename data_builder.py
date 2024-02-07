1e2334c51198239f8e7a1bfb04f3c30c5f4bfdc6
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-07 23:44:24 +0530

    Updated the data builder and merged duplicates

--- data_builder.py
+++ data_builder.py
@@ -11,11 +11,23 @@ def merge_datasets():
     return merged_df
 
 def remove_duplicates(df):
-    cols_to_drop = ['movie_title', 'plot_keywords', 'movie_imdb_link', 'language', 'country_x', 'country_y', 'content_rating', 'color', 'duration', 'facenumber_in_poster', 'movie_facebook_likes', 'num_voted_users', 'num_user_for_reviews', 'num_critic_for_reviews', 'title_year', 'aspect_ratio', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_2_facebook_likes', 'actor_1_facebook_likes', 'cast_total_facebook_likes', 'company', 'released', 'votes', 'rating', 'writer', 'year', 'runtime', 'director_name', 'genres', 'imdb_score', 'star', 'actor_3_name', 'budget_x']
+    cols_to_drop = ['movie_title', 'plot_keywords', 'movie_imdb_link', 'language', 'country_x', 'country_y', 'content_rating', 'color', 'duration', 'facenumber_in_poster', 'movie_facebook_likes', 'num_voted_users', 'num_user_for_reviews', 'num_critic_for_reviews', 'title_year', 'aspect_ratio', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_2_facebook_likes', 'actor_1_facebook_likes', 'cast_total_facebook_likes', 'company', 'released', 'votes', 'rating', 'writer', 'year', 'runtime', 'director_name', 'genres', 'imdb_score', 'star', 'actor_3_name']
     df.drop(cols_to_drop, axis=1, inplace=True)
 
     df.drop_duplicates(subset=['name'], inplace=True)
 
+    return df
+
+def add_gross_column(df):
+    if 'gross_x' not in df.columns:
+        if 'gross_y' in df.columns:
+            df['gross'] = df['gross_y']
+    return df
+
+def add_budget_column(df):
+    if 'budget_y' not in df.columns:
+        if 'budget_x' in df.columns:
+            df['budget'] = df['gross_x']
     return df
 
 def write_to_csv(df, filename):
@@ -24,7 +36,9 @@ def write_to_csv(df, filename):
 def main():
     merged_df = merge_datasets()
     cleaned_df = remove_duplicates(merged_df)
-    write_to_csv(cleaned_df, 'final_dataset.csv')
+    compared_df = add_gross_column(cleaned_df)
+    final_df = add_budget_column(compared_df)
+    write_to_csv(final_df, 'final_dataset.csv')
 
 if __name__ == "__main__":
     main()
