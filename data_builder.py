2874984db9eed90ef8ade54845417a2a5e6f7a63
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-07 23:30:37 +0530

    improved code efficiency

--- data_builder.py
+++ data_builder.py
@@ -1,19 +1,30 @@
 import pandas as pd
 
-def bulder():
-    df1 = pd.read_csv('datasets\movies.csv')
-    df2 = pd.read_csv('datasets\movie_metadata.csv')
+def merge_datasets():
+    df1 = pd.read_csv('datasets/movies.csv')
+    df2 = pd.read_csv('datasets/budget.csv')
 
     df2['movie_title'] = df2['movie_title'].str.rstrip()
 
-    merged_df = pd.merge(df1, df2, left_on='name', right_on='movie_title', suffixes=('_file1', '_file2'), how='inner')
+    merged_df = pd.merge(df1, df2, left_on='name', right_on='movie_title', how='inner')
 
-    merged_df.to_csv('result_dataset.csv', index=False)
+    return merged_df
 
-def constructor():
-    df = pd.read_csv('result_dataset.csv')
-    df = df.drop(['movie_title', 'plot_keywords', 'movie_imdb_link', 'language', 'country_file1', 'country_file2', 'content_rating', 'color', 'duration', 'facenumber_in_poster', 'movie_facebook_likes', 'num_voted_users', 'num_user_for_reviews', 'num_critic_for_reviews', 'title_year', 'aspect_ratio', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_2_facebook_likes', 'actor_1_facebook_likes', 'cast_total_facebook_likes', 'company', 'released', 'votes', 'rating', 'writer', 'year', 'runtime', 'director_name', 'genres', 'imdb_score', 'star', 'actor_3_name'], axis=1)
-    df.to_csv('final_dataset.csv', index=False)
+def remove_duplicates(df):
+    cols_to_drop = ['movie_title', 'plot_keywords', 'movie_imdb_link', 'language', 'country_file1', 'country_file2', 'content_rating', 'color', 'duration', 'facenumber_in_poster', 'movie_facebook_likes', 'num_voted_users', 'num_user_for_reviews', 'num_critic_for_reviews', 'title_year', 'aspect_ratio', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_2_facebook_likes', 'actor_1_facebook_likes', 'cast_total_facebook_likes', 'company', 'released', 'votes', 'rating', 'writer', 'year', 'runtime', 'director_name', 'genres', 'imdb_score', 'star', 'actor_3_name', 'budget_file1']
+    df.drop(cols_to_drop, axis=1, inplace=True)
 
-bulder()
-constructor()
+    df.drop_duplicates(subset=['name'], inplace=True)
+
+    return df
+
+def write_to_csv(df, filename):
+    df.to_csv(filename, index=False)
+
+def main():
+    merged_df = merge_datasets()
+    # cleaned_df = remove_duplicates(merged_df)
+    # write_to_csv(cleaned_df, 'final_dataset.csv')
+
+if __name__ == "__main__":
+    main()
