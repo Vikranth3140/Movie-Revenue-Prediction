52a2d24f5bd21b4151836853d7fc8f249f73ba9e
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-02-07 23:33:27 +0530

    Added final dataset

--- data_builder.py
+++ data_builder.py
@@ -11,7 +11,7 @@ def merge_datasets():
     return merged_df
 
 def remove_duplicates(df):
-    cols_to_drop = ['movie_title', 'plot_keywords', 'movie_imdb_link', 'language', 'country_file1', 'country_file2', 'content_rating', 'color', 'duration', 'facenumber_in_poster', 'movie_facebook_likes', 'num_voted_users', 'num_user_for_reviews', 'num_critic_for_reviews', 'title_year', 'aspect_ratio', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_2_facebook_likes', 'actor_1_facebook_likes', 'cast_total_facebook_likes', 'company', 'released', 'votes', 'rating', 'writer', 'year', 'runtime', 'director_name', 'genres', 'imdb_score', 'star', 'actor_3_name', 'budget_file1']
+    cols_to_drop = ['movie_title', 'plot_keywords', 'movie_imdb_link', 'language', 'country_x', 'country_y', 'content_rating', 'color', 'duration', 'facenumber_in_poster', 'movie_facebook_likes', 'num_voted_users', 'num_user_for_reviews', 'num_critic_for_reviews', 'title_year', 'aspect_ratio', 'director_facebook_likes', 'actor_3_facebook_likes', 'actor_2_facebook_likes', 'actor_1_facebook_likes', 'cast_total_facebook_likes', 'company', 'released', 'votes', 'rating', 'writer', 'year', 'runtime', 'director_name', 'genres', 'imdb_score', 'star', 'actor_3_name', 'budget_x']
     df.drop(cols_to_drop, axis=1, inplace=True)
 
     df.drop_duplicates(subset=['name'], inplace=True)
@@ -23,8 +23,8 @@ def write_to_csv(df, filename):
 
 def main():
     merged_df = merge_datasets()
-    # cleaned_df = remove_duplicates(merged_df)
-    # write_to_csv(cleaned_df, 'final_dataset.csv')
+    cleaned_df = remove_duplicates(merged_df)
+    write_to_csv(cleaned_df, 'final_dataset.csv')
 
 if __name__ == "__main__":
     main()
