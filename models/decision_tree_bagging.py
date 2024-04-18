45b7e73a3a37b11eaa864fa963d58080ee773400
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-04-18 16:18:58 +0530

    fixed estimator bagging model

--- models/decision_tree_bagging.py
+++ models/decision_tree_bagging.py
@@ -31,7 +31,7 @@ X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=
 base_estimator = DecisionTreeRegressor(max_depth=10, min_samples_split=10, min_samples_leaf=5, random_state=42)
 
 # Initialize BaggingRegressor with bagging parameters
-model = BaggingRegressor(base_estimator=base_estimator, n_estimators=10, random_state=42)
+model = BaggingRegressor(estimator=base_estimator, n_estimators=10, random_state=42)
 
 # Fit the model
 model.fit(X_train, y_train)
