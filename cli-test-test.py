02557fe25337cd05ae66ce536676eef2391fa859
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-05-12 16:45:55 +0530

    small fix (4)

--- cli-test-test.py
+++ cli-test-test.py
@@ -5,19 +5,18 @@ from sklearn.ensemble import GradientBoostingRegressor
 # Loading our dataset
 df = pd.read_csv('revised datasets/output.csv')
 
-# Separate categorical and numerical features
-categorical_features = ['released', 'writer', 'rating', 'name', 'genre', 'director', 'star', 'country', 'company']
-numerical_features = ['runtime', 'score', 'budget', 'year', 'votes']
+le = LabelEncoder()
 
-# Encode categorical features
-label_encoders = {}
+categorical_features = ['released', 'writer', 'rating', 'name', 'genre', 'director', 'star', 'country', 'company']
+#categorical_features = ['name', 'genre', 'director', 'star', 'country', 'company']
+
 for feature in categorical_features:
-    le = LabelEncoder()
     df[feature] = le.fit_transform(df[feature])
-    label_encoders[feature] = le
 
 # Our features and target
-features = df[categorical_features + numerical_features]
+features = df[['released', 'writer', 'rating', 'name', 'genre', 'director', 'star', 'country', 'company', 'runtime', 'score', 'budget', 'year', 'votes']]
+#features = df[['name', 'director', 'star', 'country', 'company', 'genre', 'runtime', 'score', 'budget', 'year', 'votes']]
+
 target = df['gross']
 
 # Train the model on the entire dataset
@@ -27,7 +26,7 @@ model.fit(features, target)
 # Function to predict gross for a given movie
 def predict_gross(movie_data):
     # Encode categorical features in the input data
-    for feature, le in label_encoders.items():
+    for feature in categorical_features:
         if feature in movie_data:
             movie_data[feature] = le.transform([movie_data[feature]])[0]
     
@@ -35,18 +34,12 @@ def predict_gross(movie_data):
     gross_prediction = model.predict([list(movie_data.values())])[0]
     return gross_prediction
 
-# Get input from the user for testing data
-while True:
-    movie_data = {}
-    for feature in features.columns:
-        value = input(f"Enter the value for '{feature}': ")
-        movie_data[feature] = value
-    
-    # Predict the gross for the input movie data
-    gross_prediction = predict_gross(movie_data)
-    print(f"Predicted gross for the movie: {gross_prediction}")
-    
-    # Ask if the user wants to continue testing
-    continue_testing = input("Do you want to test another movie? (yes/no): ").lower()
-    if continue_testing != 'yes':
-        break
+# Get input from the user for a single movie
+movie_data = {}
+for feature in features.columns:
+    value = input(f"Enter the value for '{feature}': ")
+    movie_data[feature] = value
+
+# Predict the gross for the input movie data
+gross_prediction = predict_gross(movie_data)
+print(f"Predicted gross for the movie: {gross_prediction}")
